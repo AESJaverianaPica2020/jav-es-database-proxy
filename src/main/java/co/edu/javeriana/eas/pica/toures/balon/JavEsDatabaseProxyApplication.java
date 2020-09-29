6cec7ec02cd9420637ae26dc15a8b833013e3ff0
ingAndresMartinez <57421413+ingAndresMartinez@users.noreply.github.com>
2020-09-28 20:52:33 -0500

    First commit
    Add man project

---
+++ src/main/java/co/edu/javeriana/eas/pica/toures/balon/JavEsDatabaseProxyApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.pica.toures.balon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEsDatabaseProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEsDatabaseProxyApplication.class, args);
+	}
+
+}
